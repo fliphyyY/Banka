e61107b0f877da1a7b2e646f38ab2617c9cdc0c8
Filip Ondrúšek <ondrusek9@stud.uniza.sk>
2019-09-11 22:12:40 +0200

    using namespace

--- Banka/Banka.cpp
+++ Banka/Banka.cpp
@@ -3,9 +3,11 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "skusam 2 ";
+    cout << "skusam 2 ";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
